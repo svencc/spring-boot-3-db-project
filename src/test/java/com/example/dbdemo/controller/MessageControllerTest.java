9a3d7b7d5a7d6cd36e6c9a089d796375efe803ee
svencc <carrillo.castillo.sven@googlemail.com>
2023-07-18 18:19:20 +0200

    re-adjust test

--- src/test/java/com/example/dbdemo/controller/MessageControllerTest.java
+++ src/test/java/com/example/dbdemo/controller/MessageControllerTest.java
@@ -3,6 +3,7 @@ package com.example.dbdemo.controller;
 import com.example.dbdemo.dto.MessageDto;
 import com.example.dbdemo.entity.Message;
 import com.example.dbdemo.repository.MessageRepository;
+import com.example.dbdemo.repository.SenderRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,15 +11,17 @@ import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
 class MessageControllerTest {
 
+    @Mock
+    private SenderRepository senderRepository;
     @Mock
     private MessageRepository messageRepository;
 
@@ -29,25 +32,26 @@ class MessageControllerTest {
     @Test
     void testListMessages() {
         // Arrange
-        Message message1 = Message.builder()
+        final Message message1 = Message.builder()
                 .id(1L)
                 .message("Hello")
                 .build();
 
-        Message message2 = Message.builder()
+        final Message message2 = Message.builder()
                 .id(2L)
                 .message("World")
                 .build();
 
-        List<Message> messages = List.of(message1, message2);
+        final List<Message> messages = List.of(message1, message2);
 
         when(messageRepository.findAll()).thenReturn(messages);
 
         // Act
-        ResponseEntity<List<MessageDto>> response = controllerUnderTest.listMessages();
+        final ResponseEntity<List<MessageDto>> response = controllerUnderTest.listMessages();
 
         // Assert
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
         assertEquals(messages.size(), response.getBody().size());
         verify(messageRepository, times(1)).findAll();
     }
@@ -55,20 +59,21 @@ class MessageControllerTest {
     @Test
     void testCreateMessage() {
         // Arrange
-        MessageDto messageDto = new MessageDto();
+        final MessageDto messageDto = new MessageDto();
         messageDto.setMessage("Test Message");
 
-        Message savedMessage = new Message();
+        final Message savedMessage = new Message();
         savedMessage.setId(1L);
         savedMessage.setMessage(messageDto.getMessage());
 
         when(messageRepository.save(any(Message.class))).thenReturn(savedMessage);
 
         // Act
-        ResponseEntity<MessageDto> response = controllerUnderTest.createMessage(messageDto);
+        final ResponseEntity<MessageDto> response = controllerUnderTest.createMessage(messageDto);
 
         // Assert
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
         assertEquals(savedMessage.getId(), response.getBody().getId());
         assertEquals(savedMessage.getMessage(), response.getBody().getMessage());
         verify(messageRepository, times(1)).save(any(Message.class));
